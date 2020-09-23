2be8ee7a98a665604d340f1c8b3543a5bdab8ae5
Qasim Siddiqui <m.qasim678@gmail.com>
2020-09-24 01:50:25 +0500

    app name added

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Movies App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
