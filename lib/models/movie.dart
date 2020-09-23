d362df6db6f830419b8192c1fc9072c6531ee5d1
Qasim Siddiqui <m.qasim678@gmail.com>
2020-09-24 00:34:12 +0500

    movie and searchresults classes updated

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,26 +1,130 @@
 class Movie {
-  String title;
-  String releaseDate;
-  String id;
-  String avgVote;
-  String posterPath;
-  String overView;
+  int _id;
+  bool _video;
+  int _voteCount;
+  double _voteAverage;
+  String _title;
+  String _releaseDate;
+  String _originalLanguage;
+  String _originalTitle;
+  List<int> _genreIds;
+  String _backdropPath;
+  bool _adult;
+  String _overview;
+  String _posterPath;
+  double _popularity;
+  String _mediaType;
+  String _originalName;
+  String _name;
+  String _firstAirDate;
+  List<String> _originCountry;
 
-  Movie.initialValue()
-      : title = "",
-        releaseDate = "",
-        id = "",
-        avgVote = "",
-        posterPath = "",
-        overView = "";
+  Movie(
+      {int id,
+      bool video,
+      int voteCount,
+      double voteAverage,
+      String title,
+      String releaseDate,
+      String originalLanguage,
+      String originalTitle,
+      List<int> genreIds,
+      String backdropPath,
+      bool adult,
+      String overview,
+      String posterPath,
+      double popularity,
+      String mediaType,
+      String originalName,
+      String name,
+      String firstAirDate,
+      List<String> originCountry}) {
+    this._id = id;
+    this._video = video;
+    this._voteCount = voteCount;
+    this._voteAverage = voteAverage;
+    this._title = title;
+    this._releaseDate = releaseDate;
+    this._originalLanguage = originalLanguage;
+    this._originalTitle = originalTitle;
+    this._genreIds = genreIds;
+    this._backdropPath = backdropPath;
+    this._adult = adult;
+    this._overview = overview;
+    this._posterPath = posterPath;
+    this._popularity = popularity;
+    this._mediaType = mediaType;
+    this._originalName = originalName;
+    this._name = name;
+    this._firstAirDate = firstAirDate;
+    this._originCountry = originCountry;
+  }
 
-  Movie.fromJson(Map<String, dynamic> json)
-      : title = json["title"],
-        releaseDate = json["release_date"],
-        id = json["id"].toString(),
-        avgVote = json["vote_average"].toString(),
-        posterPath = json["poster_path"],
-        overView = json["overview"];
+  int get id => _id;
+  bool get video => _video;
+  int get voteCount => _voteCount;
+  double get voteAverage => _voteAverage;
+  String get title => _title;
+  String get releaseDate => _releaseDate;
+  String get originalLanguage => _originalLanguage;
+  String get originalTitle => _originalTitle;
+  List<int> get genreIds => _genreIds;
+  String get backdropPath => _backdropPath;
+  bool get adult => _adult;
+  String get overview => _overview;
+  String get posterPath => _posterPath;
+  double get popularity => _popularity;
+  String get mediaType => _mediaType;
+  String get originalName => _originalName;
+  String get name => _name;
+  String get firstAirDate => _firstAirDate;
+  List<String> get originCountry => _originCountry;
+
+  Movie.fromJson(Map<String, dynamic> json) {
+    _id = json['id'];
+    _video = json['video'];
+    _voteCount = json['vote_count'];
+    _voteAverage = json['vote_average'];
+    _title = json['title'];
+    _releaseDate = json['release_date'];
+    _originalLanguage = json['original_language'];
+    _originalTitle = json['original_title'];
+    _genreIds = json['genre_ids'].cast<int>();
+    _backdropPath = json['backdrop_path'];
+    _adult = json['adult'];
+    _overview = json['overview'];
+    _posterPath = json['poster_path'];
+    _popularity = json['popularity'];
+    _mediaType = json['media_type'];
+    _originalName = json['original_name'];
+    _name = json['name'];
+    _firstAirDate = json['first_air_date'];
+    _originCountry = json['origin_country'].cast<String>();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this._id;
+    data['video'] = this._video;
+    data['vote_count'] = this._voteCount;
+    data['vote_average'] = this._voteAverage;
+    data['title'] = this._title;
+    data['release_date'] = this._releaseDate;
+    data['original_language'] = this._originalLanguage;
+    data['original_title'] = this._originalTitle;
+    data['genre_ids'] = this._genreIds;
+    data['backdrop_path'] = this._backdropPath;
+    data['adult'] = this._adult;
+    data['overview'] = this._overview;
+    data['poster_path'] = this._posterPath;
+    data['popularity'] = this._popularity;
+    data['media_type'] = this._mediaType;
+    data['original_name'] = this._originalName;
+    data['name'] = this._name;
+    data['first_air_date'] = this._firstAirDate;
+    data['origin_country'] = this._originCountry;
+    return data;
+  }
 
   @override
   String toString() {
--- lib/models/search_results.dart
+++ lib/models/search_results.dart
@@ -1,24 +1,44 @@
 import 'package:movies_app/models/movie.dart';
 
 class SearchResult {
-  List movieList;
-  int pageNo;
-  int totalPages;
-  int totalResults;
+  int _page;
+  List<Movie> _movieList;
+  int _totalPages;
+  int _totalResults;
 
-  SearchResult.fromJson(Map<String, dynamic> json)
-      : totalResults = json["total_results"],
-        totalPages = json["total_pages"],
-        pageNo = json["page"],
-        movieList = json["results"].map((movie) {
-          return Movie.fromJson(movie);
-        }).toList();
+  SearchResult(
+      {int page, List<Movie> movieList, int totalPages, int totalResults}) {
+    this._page = page;
+    this._movieList = movieList;
+    this._totalPages = totalPages;
+    this._totalResults = totalResults;
+  }
 
-  @override
-  String toString() {
-    return """Search Result: \n
-        totalPages: $totalPages \t totalResults: $totalResults \t pageNo: $pageNo \n
-        movieList: ${movieList.length}   
-    """;
+  int get page => _page;
+  List<Movie> get movieList => _movieList;
+  int get totalPages => _totalPages;
+  int get totalResults => _totalResults;
+
+  SearchResult.fromJson(Map<String, dynamic> json) {
+    _page = json['page'];
+    if (json['results'] != null) {
+      _movieList = new List<Movie>();
+      json['results'].forEach((v) {
+        _movieList.add(new Movie.fromJson(v));
+      });
+    }
+    _totalPages = json['total_pages'];
+    _totalResults = json['total_results'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['page'] = this._page;
+    if (this._movieList != null) {
+      data['results'] = this._movieList.map((v) => v.toJson()).toList();
+    }
+    data['total_pages'] = this._totalPages;
+    data['total_results'] = this._totalResults;
+    return data;
   }
 }
