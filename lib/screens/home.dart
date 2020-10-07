286d6cdf4c016eece58fce2a3538d42029788109
Qasim Siddiqui <m.qasim678@gmail.com>
2020-10-07 22:04:06 +0500

    refactored trending movie carousel

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:movies_app/api/api.dart';
-import 'package:movies_app/models/search_results.dart';
 import 'package:movies_app/widgets/movie_details_carousel.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -21,45 +19,7 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
       ),
       body: Column(
-        children: [
-          FutureBuilder(
-              future: API().trendingMovies(),
-              builder: (_, AsyncSnapshot<SearchResult> snapshot) {
-                List<Widget> children;
-                if (snapshot.hasData) {
-                  children = <Widget>[
-                    MovieDetailsCarousel(snapshot.data),
-                  ];
-                } else if (snapshot.hasError) {
-                  children = <Widget>[
-                    Icon(
-                      Icons.error_outline,
-                      color: Colors.red,
-                      size: 60,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 16),
-                      child: Text('Error: ${snapshot.error}'),
-                    )
-                  ];
-                } else {
-                  children = <Widget>[
-                    SizedBox(
-                      child: CircularProgressIndicator(),
-                      width: 60,
-                      height: 60,
-                    )
-                  ];
-                }
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: children,
-                  ),
-                );
-              }),
-        ],
+        children: [TrendingMoviesCarousel()],
       ),
     );
   }
