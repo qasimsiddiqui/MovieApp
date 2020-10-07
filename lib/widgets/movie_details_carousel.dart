8c767687d520ea54718a4262d3492e040e668f99
Qasim Siddiqui <m.qasim678@gmail.com>
2020-10-07 22:02:48 +0500

    redesigned trending movies carousel

--- lib/widgets/movie_details_carousel.dart
+++ lib/widgets/movie_details_carousel.dart
@@ -1,47 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:movies_app/api/api.dart';
 import 'package:movies_app/api/api_configuration.dart';
 import 'package:movies_app/models/search_results.dart';
 import 'package:movies_app/screens/movie_details.dart';
-import 'package:smooth_star_rating/smooth_star_rating.dart';
 
-class MovieDetailsCarousel extends StatelessWidget {
-  final SearchResult _result;
+class TrendingMoviesCarousel extends StatefulWidget {
+  @override
+  _TrendingMoviesCarouselState createState() => _TrendingMoviesCarouselState();
+}
 
-  MovieDetailsCarousel(this._result);
-
+class _TrendingMoviesCarouselState extends State<TrendingMoviesCarousel> {
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider.builder(
-      itemCount: _result.movieList.length ?? 0,
-      options: CarouselOptions(
-          autoPlay: true,
-          autoPlayInterval: Duration(seconds: 3),
-          autoPlayAnimationDuration: Duration(milliseconds: 500),
-          height: 350,
-          viewportFraction: 0.7),
-      itemBuilder: (BuildContext context, int itemIndex) => Card(
-        color: Colors.white70,
-        margin: EdgeInsets.all(4),
-        elevation: 2,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: InkWell(
-          splashColor: Colors.blue.withAlpha(30),
-          onTap: () => Navigator.push(
-              context,
-              new MaterialPageRoute(
-                  builder: (context) => MovieDetailsScreen(
-                      movieID: _result.movieList[itemIndex].id))),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: _PosterImage(
-              result: _result,
-              itemIndex: itemIndex,
+    return FutureBuilder(
+        future: API().trendingMovies(),
+        builder: (_, AsyncSnapshot<SearchResult> snapshot) {
+          List<Widget> children;
+          if (snapshot.hasData) {
+            children = <Widget>[
+              CarouselSlider.builder(
+                itemCount: snapshot.data.movieList.length ?? 0,
+                options: CarouselOptions(
+                    autoPlay: true,
+                    autoPlayInterval: Duration(seconds: 3),
+                    autoPlayAnimationDuration: Duration(milliseconds: 500),
+                    height: MediaQuery.of(context).size.height * 0.6,
+                    viewportFraction: 0.7),
+                itemBuilder: (BuildContext context, int itemIndex) => Card(
+                  color: Colors.white70,
+                  margin: EdgeInsets.all(4),
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: InkWell(
+                    splashColor: Colors.blue.withAlpha(30),
+                    onTap: () => Navigator.push(
+                        context,
+                        new MaterialPageRoute(
+                            builder: (context) => MovieDetailsScreen(
+                                movieID:
+                                    snapshot.data.movieList[itemIndex].id))),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: _PosterImage(
+                        result: snapshot.data,
+                        itemIndex: itemIndex,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ];
+          } else if (snapshot.hasError) {
+            children = <Widget>[
+              Icon(
+                Icons.error_outline,
+                color: Colors.red,
+                size: 60,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text('Error: ${snapshot.error}'),
+              )
+            ];
+          } else {
+            children = <Widget>[
+              SizedBox(
+                child: CircularProgressIndicator(),
+                width: 60,
+                height: 60,
+              )
+            ];
+          }
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: children,
             ),
-          ),
-        ),
-      ),
-    );
+          );
+        });
   }
 }
 
