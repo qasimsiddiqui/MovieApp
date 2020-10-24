f07161cfe5943ea934930525a8debec6059939fa
qasimsiddiqui <qasim.siddiqui678@gmail.com>
2020-10-25 01:33:43 +0500

    Search icon added to appbar, debug banner removed

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,8 +16,22 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: new Color.fromRGBO(27, 33, 47, 1),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        title: Text('Movies App'),
+        title: Text(
+          'Movies App',
+          style: Theme.of(context).textTheme.headline5,
+        ),
         centerTitle: true,
+        actions: [
+          IconButton(
+            padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+            icon: Icon(
+              Icons.search,
+              size: 35,
+              color: Colors.white,
+            ),
+            onPressed: null,
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
