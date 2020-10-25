13ee9149e0704df96320933e4c67c95086b51fd8
qasimsiddiqui <qasim.siddiqui678@gmail.com>
2020-10-25 20:51:59 +0500

    basic try_catch blocks added to API class

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -6,56 +6,84 @@ import 'dart:convert';
 
 class API {
   Future<SearchResult> searchMovie(String movieName) async {
-    var response = await EndPoints().searchMovie(movieName);
-    //if OK response and data retrieved
-    if (response.statusCode == 200) {
-      return SearchResult.fromJson(json.decode(response.body));
-    } else {
-      print(response.body);
+    try {
+      var response = await EndPoints().searchMovie(movieName);
+
+      //if OK response and data retrieved
+      if (response.statusCode == 200) {
+        return SearchResult.fromJson(json.decode(response.body));
+      } else {
+        print(response.body);
+        return null;
+      }
+    } catch (ex) {
+      print(ex.toString());
       return null;
     }
   }
 
   Future<SearchResult> trendingMovies() async {
-    var response = await EndPoints().trendingMovies();
-    //if OK response and data retrieved
-    if (response.statusCode == 200) {
-      return SearchResult.fromJson(json.decode(response.body));
-    } else {
-      print(response.body);
+    try {
+      var response = await EndPoints().trendingMovies();
+      //if OK response and data retrieved
+      if (response.statusCode == 200) {
+        return SearchResult.fromJson(json.decode(response.body));
+      } else {
+        print(response.body);
+        return null;
+      }
+    } catch (ex) {
+      print(ex.toString());
       return null;
     }
   }
 
   Future<SearchResult> getKeywordMovies(String keyword) async {
-    var response = await EndPoints().getKeywordMovies(keyword);
-    //if OK response and data retrieved
-    if (response.statusCode == 200) {
-      return SearchResult.fromJson(json.decode(response.body));
-    } else {
-      print(response.body);
+    try {
+      var response = await EndPoints().getKeywordMovies(keyword);
+
+      //if OK response and data retrieved
+      if (response.statusCode == 200) {
+        return SearchResult.fromJson(json.decode(response.body));
+      } else {
+        print(response.body);
+        return null;
+      }
+    } catch (ex) {
+      print(ex.toString());
       return null;
     }
   }
 
   Future<MovieDetails> getMovieDetails(int movieID) async {
-    var response = await EndPoints().getMovieDetails(movieID);
-    //if OK response and data retrieved
-    if (response.statusCode == 200) {
-      return MovieDetails.fromJson(json.decode(response.body));
-    } else {
-      print("Error : " + response.body);
+    try {
+      var response = await EndPoints().getMovieDetails(movieID);
+
+      //if OK response and data retrieved
+      if (response.statusCode == 200) {
+        return MovieDetails.fromJson(json.decode(response.body));
+      } else {
+        print("Error : " + response.body);
+        return null;
+      }
+    } catch (ex) {
+      print(ex.toString());
       return null;
     }
   }
 
   Future<MovieCredits> getCrewAndCast(int movieID) async {
-    var response = await EndPoints().getMovieCredits(movieID);
-    //if OK response and data retrieved
-    if (response.statusCode == 200) {
-      return MovieCredits.fromJson(json.decode(response.body));
-    } else {
-      print("Error : " + response.body);
+    try {
+      var response = await EndPoints().getMovieCredits(movieID);
+      //if OK response and data retrieved
+      if (response.statusCode == 200) {
+        return MovieCredits.fromJson(json.decode(response.body));
+      } else {
+        print("Error : " + response.body);
+        return null;
+      }
+    } catch (ex) {
+      print(ex.toString());
       return null;
     }
   }
