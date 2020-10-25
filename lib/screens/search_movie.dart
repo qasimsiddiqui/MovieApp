1900c835ac7f42f71979414c201b09a25870a5a9
qasimsiddiqui <qasim.siddiqui678@gmail.com>
2020-10-25 17:16:40 +0500

    search TextField designed

--- lib/screens/search_movie.dart
+++ lib/screens/search_movie.dart
@@ -6,8 +6,66 @@ class SearchMovie extends StatefulWidget {
 }
 
 class _SearchMovieState extends State<SearchMovie> {
+  String searchText = "";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: new Color.fromRGBO(27, 33, 47, 1),
+      appBar: AppBar(
+        title: Text(
+          "Search",
+          style: Theme.of(context)
+              .textTheme
+              .headline5
+              .copyWith(fontWeight: FontWeight.w700, fontSize: 28),
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+              child: TextFormField(
+                style: TextStyle(fontSize: 20, color: Colors.white),
+                keyboardType: TextInputType.text,
+                textCapitalization: TextCapitalization.words,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    iconSize: 28,
+                    onPressed: () {
+                      print(searchText);
+                    },
+                  ),
+                  suffixIconConstraints:
+                      BoxConstraints(minHeight: 5, minWidth: 35),
+                  isDense: true,
+                  contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  hintText: "Search Movies",
+                  hintStyle: TextStyle(fontSize: 20, color: Colors.white30),
+                  filled: true,
+                  fillColor: const Color(0xFF273347),
+                  focusColor: Colors.white,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide.none,
+                      gapPadding: 2),
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    searchText = value;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
